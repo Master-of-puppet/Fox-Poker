b0c893dac69aeb05f773937b26d8c0533f92a638
cong91 <thanhcong191@gmail.com>
2014-12-15 00:18:31 +0700

    Add Local Push notification

---
+++ Assets/ThirdParty/PushNotification/AndroidLocalPushNotification.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AndroidLocalPushNotification  {
+	private static AndroidJavaObject _localPush;
+	
+	
+	static AndroidLocalPushNotification()
+	{
+		if( Application.platform != RuntimePlatform.Android )
+			return;
+		
+		// find the plugin instance
+		using( var pluginClass = new AndroidJavaClass( "com.puppet.congnt.localpushnotification.PuppetPlugin" ) )
+			_localPush = pluginClass.CallStatic<AndroidJavaObject>( "getInstance" );
+		
+	}
+
+	public static void sendPushNotification(int second,string title,string message){
+		if( Application.platform != RuntimePlatform.Android )
+			return;
+		_localPush.Call( "sendPushNotification",title,message,second);
+	}
+    public static void clearLocalPushNotification()
+    {
+        if (Application.platform != RuntimePlatform.Android)
+            return;
+        _localPush.Call("clearPushNotification");
+    }
+}
