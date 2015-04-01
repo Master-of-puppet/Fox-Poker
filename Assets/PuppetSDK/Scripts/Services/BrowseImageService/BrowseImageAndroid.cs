274ea3db58a9fc25a66dc370e2fee3aee53ba90e
Nguyen Viet Dung - Seta <dungnv6414@seta-international.vn>
2015-04-01 21:14:23 +0700

    Add API BrowseImageFromGallery

---
+++ Assets/PuppetSDK/Scripts/Services/BrowseImageService/BrowseImageAndroid.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+class BrowseImageAndroid : AbstractAndroidAPI, IBrowseImage
+{
+    protected override string ClassAndroidAPI
+    {
+        get { return "com.ensign.browseimagefromgallery.ImageAPI"; }
+    }
+
+    public void GetImageFromGallery(string objectName, string method, string cacheFileName)
+    {
+#if UNITY_ANDROID && !UNITY_EDITOR
+         Call("dungnv_GetImageFromGallery", new object[] { objectName, method, cacheFileName });
+#endif
+    }
+}
