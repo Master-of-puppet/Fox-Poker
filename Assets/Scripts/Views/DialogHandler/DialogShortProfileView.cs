1b2cc2599667795a0c0596306e8539cfd935429d
cong91 <thanhcong191@gmail.com>
2015-04-05 21:47:56 +0700

    them dialog list qua tang

--- Assets/Scripts/Views/DialogHandler/DialogShortProfileView.cs
+++ Assets/Scripts/Views/DialogHandler/DialogShortProfileView.cs
@@ -6,18 +6,26 @@ using Puppet;
 using Puppet.Core.Network.Http;
 using Puppet.Poker.Models;
 
-[PrefabAttribute(Name = "Prefabs/Dialog/UserInfo/DialogShortProfile", Depth = 10, IsAttachedToCamera = true, IsUIPanel = true)]
+[PrefabAttribute(Name = "Prefabs/Dialog/UserInfo/DialogShortProfile", Depth = 7, IsAttachedToCamera = true, IsUIPanel = true)]
 public class DialogShortProfileView : BaseDialog<DialogShortProfile, DialogShortProfileView> {
 	#region UnityEditor
 	public UILabel lbUserName,lbChip,lbLevel;
 	public UITexture avatar;
+    public GameObject btnGift, btnItem;
 	#endregion
 	protected override void OnEnable ()
 	{
+        UIEventListener.Get(btnGift).onClick += onClickButtonGift;
 		base.OnEnable ();
 	}
+
+    private void onClickButtonGift(GameObject go)
+    {
+        DialogService.Instance.ShowDialog(new DialogListGift());
+    }
 	protected override void OnDisable ()
 	{
+        UIEventListener.Get(btnGift).onClick -= onClickButtonGift;
 		base.OnDisable ();
 	}
 	public override void ShowDialog (DialogShortProfile data)
